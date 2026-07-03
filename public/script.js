032714dcd391a699a5b8289a40cdfef88da0a469
Rutger van den Berg <rutgervandenberg@Rutgers-MacBook-Air.local>
2026-07-03 13:38:30 +0200

    bugfix

--- public/script.js
+++ public/script.js
@@ -9,8 +9,8 @@ function getMonday(d) {
     d = new Date(d);
     var day = d.getDay(), diff = d.getDate() - day + (day == 0 ? -6 : 1);
     return new Date(d.setDate(diff));
-    let viewingWeekStart = getMonday(new Date());
 }
+let viewingWeekStart = getMonday(new Date());
 
 let globalMilestones = [];
 let globalMetrics = [];
